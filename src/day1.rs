d4f2b8d3084f69e31445d12da6a10cdce9e8d747
Caleb Jasik <calebjasik@jasik.xyz>
2018-12-01 10:48:51 -0600

    Update the comments to reflect code a tiny bit better

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+
+// Nice and easy, lemon squeezy.
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
     input
@@ -16,8 +18,8 @@ pub fn solve_part1(input: &[i32]) -> i32 {
 }
 
 // So, I think that I probably need to take the current frequency each time and compare it to the
-// previous ones. Ooh yeah, I figured it out - we can just append each element to a new Vec<i32> that
-// will now contain all previous sums.
+// previous ones. Ooh yeah, I figured it out - we can just append each element to a new HashSet<i32>
+// that will now contain all previous sums.
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &[i32]) -> i32 {
     let mut sum: i32 = 0;

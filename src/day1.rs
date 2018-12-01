6318cc40eac48afc27e018c5f6c43df8d7bec4d0
Caleb Jasik <calebjasik@jasik.xyz>
2018-12-01 05:54:53 -0600

    Completed parts 1 and 2 of day1

---
+++ src/day1.rs
@@ -0,0 +1,33 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<i32> {
+    input
+        .lines()
+        .map(|l| {
+            let num = l.trim().parse();
+            num.unwrap()
+        }).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[i32]) -> i32 {
+    input.iter().sum()
+}
+
+// So, I think that I probably need to take the current frequency each time and compare it to the
+// previous ones. Ooh yeah, I figured it out - we can just append each element to a new Vec<i32> that
+// will now contain all previous sums.
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[i32]) -> i32 {
+    let mut sum: i32 = 0;
+    let mut seen: Vec<i32> = Vec::new();
+    let mut index: usize = 0;
+    while !seen.contains(&sum) {
+        seen.push(sum);
+        sum += input[index];
+        index += 1;
+        if index >= input.len() {
+            index = 0;
+        }
+    }
+    sum
+}

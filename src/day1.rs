03bffd73072d0a8cd4fd6b429465ac6b6b31d89c
Caleb Jasik <calebjasik@jasik.xyz>
2018-12-01 18:37:17 -0600

    Create two extra named solutions to day1, part2 to compare different hashes

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,11 @@
-//use std::collections::HashSet;
+use std::collections::HashSet;
 //use std::iter::repeat;
+extern crate fnv;
 use fnv::FnvHashSet;
+
+extern crate hashbrown;
+
+use hashbrown::HashSet as HashbrownHashSet;
 
 // Nice and easy, lemon squeezy.
 #[aoc_generator(day1)]
@@ -21,10 +26,42 @@ pub fn solve_part1(input: &[i32]) -> i32 {
 // So, I think that I probably need to take the current frequency each time and compare it to the
 // previous ones. Ooh yeah, I figured it out - we can just append each element to a new HashSet<i32>
 // that will now contain all previous sums.
-#[aoc(day1, part2)]
-pub fn solve_part2(input: &[i32]) -> i32 {
+#[aoc(day1, part2, StdCollections)]
+pub fn solve_part2_std(input: &[i32]) -> i32 {
+    let mut sum: i32 = 0;
+    let mut seen: HashSet<i32> = HashSet::new();
+    let mut index: usize = 0;
+    while !seen.contains(&sum) {
+        seen.insert(sum);
+        sum += input[index];
+        index += 1;
+        if index >= input.len() {
+            index = 0;
+        }
+    }
+    sum
+}
+
+#[aoc(day1, part2, Fnv)]
+pub fn solve_part2_fnv(input: &[i32]) -> i32 {
     let mut sum: i32 = 0;
     let mut seen: FnvHashSet<i32> = FnvHashSet::default();
+    let mut index: usize = 0;
+    while !seen.contains(&sum) {
+        seen.insert(sum);
+        sum += input[index];
+        index += 1;
+        if index >= input.len() {
+            index = 0;
+        }
+    }
+    sum
+}
+
+#[aoc(day1, part2, Hashbrown)]
+pub fn solve_part2_hashbrown(input: &[i32]) -> i32 {
+    let mut sum: i32 = 0;
+    let mut seen: HashbrownHashSet<i32> = HashbrownHashSet::new();
     let mut index: usize = 0;
     while !seen.contains(&sum) {
         seen.insert(sum);

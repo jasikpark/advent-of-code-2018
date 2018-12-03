399b986a544cd455e758b07e2e95e28f97a0c787
Caleb Jasik <calebjasik@jasik.xyz>
2018-12-03 15:41:56 -0600

    Make the iteration return a map variable instead of ()

--- src/day2.rs
+++ src/day2.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 #[aoc(day2, part1)]
 pub fn solve_part1(input: &str) -> i32 {
-    let chars_count_iter: () = input
+    let _chars_count_iter = input
         .lines()
         .map(|word: &str| {
             word.chars().fold(HashMap::new(), |mut char_counts, c| {
@@ -11,7 +11,6 @@ pub fn solve_part1(input: &str) -> i32 {
                 char_counts
             })
         })
-        .iter()
-        .map(|char_counts: HashMap<char, usize>| char_counts.iter().filter(&|_, count| count == 2));
+        .map(|char_counts: HashMap<char, usize>| char_counts.iter().filter(|(_, count)| **count == 2usize));
     3
 }
